35af77f63998e9a03d4b22a86314699a0296f63d
damian lonel <damian@playworkstudios.com.au>
2021-02-25 20:39:46 +1100

    select2

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -12,7 +12,7 @@ class DosesController < ApplicationController
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @ingredient = Ingredient.find(params[:ingredient_id])
+    # @ingredient = Ingredient.find(params[:ingredient_id])
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
