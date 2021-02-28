3c80cf9006f7d548756b2482307b2d2e298b2554
damian lonel <damian@playworkstudios.com.au>
2021-02-28 16:13:23 +1100

    updates 28feb

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,6 +5,8 @@ class CocktailsController < ApplicationController
     @cocktails = Cocktail.all
   end
 
+
+
   def show
     @dose = Dose.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/search'
   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
       resources :doses, only: [:new, :create]
     end
   resources :doses, only: [:update, :destroy]
   resources :ingredients, only: [:index, :new, :create, :update, :destroy]
+
+get '/search' => 'pages_controller#search', :as => 'search_page'
 end
